1ceb2bcce4ded2b0b25df5a6a842367800f0aa4d
Davis <davis-wolfermann2005@gmx.de>
2024-05-24 17:05:19 +0200

    [Gradle] Updated for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     `kotlin-dsl`
+    alias(libs.plugins.gradle.pluginPublish)
 }
+
+group = "de.davis"
+version = "0.1.0"
 
 dependencies {
     implementation(libs.semantic.versioning)
@@ -9,9 +13,16 @@ dependencies {
     implementation(libs.android.gradleApi)
 }
 
+@Suppress("UnstableApiUsage")
 gradlePlugin {
+    website = "https://github.com/OffRange/git-semver-gradle-plugin"
+    vcsUrl = "https://github.com/OffRange/git-semver-gradle-plugin.git"
     plugins {
         create("versioning") {
+            displayName = "Gradle Git Semantic Versioning Plugin"
+            description =
+                "A Gradle plugin that automatically versions your project based on Git tags, following the Semantic Versioning 2.0 specification."
+            tags = listOf("git", "vcs", "semantic version", "semver", "versioning")
             id = "de.davis.git-semantic-versioning"
             implementationClass = "de.davis.gradle.plugin.versioning.VersioningPlugin"
         }
