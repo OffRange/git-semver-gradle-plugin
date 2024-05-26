cc08010de4bb976ac4126901cfe7fda40675cb8b
Davis <davis-wolfermann2005@gmx.de>
2024-05-26 18:11:57 +0200

    [GradleProperty] Removed OptionalGradleProperty

--- src/main/kotlin/de/davis/gradle/plugin/versioning/GradleProperty.kt
+++ src/main/kotlin/de/davis/gradle/plugin/versioning/GradleProperty.kt
@@ -12,17 +12,5 @@ internal class GradleProperty<V : Any>(objectFactory: ObjectFactory, type: Class
     operator fun setValue(thisRef: Any, kProperty: KProperty<*>, value: V) = property.set(value)
 }
 
-internal class OptionalGradleProperty<V : Any>(objectFactory: ObjectFactory, type: Class<V>, defaultValue: V? = null) {
-    private val property = objectFactory.property(type).apply {
-        set(defaultValue)
-    }
-
-    operator fun getValue(thisRef: Any, kProperty: KProperty<*>): V? = property.orNull
-    operator fun setValue(thisRef: Any, kProperty: KProperty<*>, value: V?) = property.set(value)
-}
-
 internal inline fun <reified T : Any> ObjectFactory.createGradleProperty(defaultValue: T? = null): GradleProperty<T> =
     GradleProperty(this, T::class.java, defaultValue)
-
-internal inline fun <reified T : Any> ObjectFactory.createOptionalGradleProperty(defaultValue: T? = null): OptionalGradleProperty<T> =
-    OptionalGradleProperty(this, T::class.java, defaultValue)
